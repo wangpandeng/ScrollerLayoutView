07ce576fa600289c491ac512dec900484c9ebb0f
wangpandeng <wangpandeng0527@163.com>
2016-07-11 18:02:23 +0800

    第一次修改

--- app/src/main/java/com/example/wang/wangpd_demo/MainActivity.java
+++ app/src/main/java/com/example/wang/wangpd_demo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.wang.wangpd_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("nihao");
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
